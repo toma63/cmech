5421ffc499081523bfd70908741e4aefacf7cc50
toma <t_arns@yahoo.com>
2016-01-23 16:39:36 -0800

    added system_demo for GetCMSystem

---
+++ getsv/system_demo/system_demo.go
@@ -0,0 +1,31 @@
+package main
+
+import ("fmt"
+	"github.com/toma63/cmech/getsv"
+	"time"
+	"flag"
+	"regexp"
+	"strings"
+)
+
+
+
+// main function
+func main() {
+	numre := regexp.MustCompile(`^\d+$`)
+	objects := flag.String("objects", "", "Colon separated list of names of a celestial objects")
+	flag.Parse()
+	onames := strings.Split(*objects, ":")
+	now := time.Now()
+	system := getsv.GetCMSystem(onames, now)
+
+	for _, body := range system {
+		horznum := ""
+		if numre.MatchString(body.Name) {
+			horznum = "JPL Horizons object "
+		}
+		fmt.Printf("Mass of %s%s (kg): %e\n", horznum, body.Name, body.Mass)
+		fmt.Printf("Position (km): x: %e y: %e z: %e\n", body.X, body.Y, body.Z)
+		fmt.Printf("Velocity (km/s): x: %e y: %e z: %e\n", body.Vx, body.Vy, body.Vz)
+	}
+}
